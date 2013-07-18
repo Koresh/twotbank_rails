fc894c74961c2f026f1d995ad2185fd86c7353cb
Koresh <koresh.x9@gmail.com>
2013-07-18 15:22:31 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwotbankRails::Application.config.secret_key_base = 'c034b3be81a5cb80258c2342e8af87bc473812857376b80632d66df7fb9a7d72e0f7b19d222c95a349f2e3527c13557700860cb7efb5ca91c428f3b99832454c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwotbankRails::Application.config.session_store :cookie_store, key: '_twotbank_rails_session'
