aa67da55fffd4a58b623b885048bc0b0e7d680d8
av <av@fun-box.ru>
2013-09-10 19:27:58 +0400

    Created BusinessController actions

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,4 +1,36 @@
 class BusinessController < ApplicationController
+
   def index
   end
+
+  def acquiring
+  end
+
+  def business_card
+  end
+
+  def dohodniy_schet_dlya_biznesa
+  end
+
+  def encashment_card
+  end
+
+  def internet_acquiring
+  end
+
+  def overdraft
+  end
+
+  def salary
+  end
+
+  def schet_dlya_biznesa
+  end
+
+  def sms
+  end
+
+  def vklad_na_srok
+  end
+
 end
